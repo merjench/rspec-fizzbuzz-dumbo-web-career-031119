359c7220153c95078efa36f5aeb8e7266a330139
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-05 07:02:15 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
-  if int % 3 == 0 and int % 5 == 0
+def fizzbuzz(number)
+  if number % 3 == 0 and number % 5 == 0
     return "FizzBuzz"
-  elsif int % 5 == 0
+  elsif number % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0
+  elsif number % 3 == 0
     return  "Fizz"
-
    end
-
  end
